46c810aaedaf54a1166cb71cd2049a1d5aab1374
Aram Kocharyan <akarmenia@gmail.com>
2014-12-01 23:53:33 +1100

    Migrated to d3js:d3.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 // Meteor package definition.
 Package.describe({
   name: 'aramk:vega-charts',
-  version: '0.2.1',
+  version: '0.2.2',
   summary: 'Simple charting with Vega and D3.',
   git: 'https://github.com/aramk/vega-charts.git'
 });
@@ -16,7 +16,7 @@ Package.onUse(function (api) {
     'underscore',
     'aramk:q@1.0.1',
     'aramk:utility@0.3.0',
-    'aramk:vega@1.4.2'
+    'aramk:vega@1.4.2_1'
     ],'client');
   api.imply('aramk:vega');
   // TODO(aramk) Perhaps expose the charts through the Vega object only to avoid cluttering the
